761c7118e24291bf303980ddfc643f453add799b
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-01-22 18:21:17 -0600

    Made stack

--- src/main/java/org/stack/Stack.java
+++ src/main/java/org/stack/Stack.java
@@ -17,4 +17,29 @@ public class Stack<T> {
 
     private boolean isFull() { return size == capacity; }
 
+    public boolean push (T newElement) {
+        if (isFull()) return false;
+        stack[size++] = newElement;
+        return true;
+    }
+
+    public T pop() {
+        if (isEmpty()) return null;
+        return stack[--size];
+    }
+
+    public T peek() {
+        if (isEmpty()) return null;
+        return stack[size - 1];
+    }
+
+    public void printAll() {
+        if (isEmpty()) {
+            System.out.println(stack.getClass().getName() +" Stack Is Currently Empty");
+            return;
+        }
+        for (int i = size; i >= 0; i--)
+            System.out.println(stack[i]);
+    }
+
 }
