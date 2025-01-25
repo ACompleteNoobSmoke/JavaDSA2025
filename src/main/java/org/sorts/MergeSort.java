e593b29a575f8de74b23946042ea7930544051ef
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-01-24 20:49:39 -0600

    Created Merge Sort

--- src/main/java/org/sorts/MergeSort.java
+++ src/main/java/org/sorts/MergeSort.java
@@ -1,69 +1,43 @@
 package org.sorts;
 
-import java.util.Random;
+public class MergeSort<T extends Comparable<T>> {
 
-public class MergeSort {
+    public MergeSort() {
 
-    private int[] randomNums(int capacity) {
-        int[] arr = new int[capacity];
-        Random rand = new Random();
-        for (int i = 0; i < capacity; i++) {
-            arr[i] = rand.nextInt(capacity);
-        }
-        return arr;
     }
 
-    private void printArr(int[] arr) {
-        for (int j : arr) System.out.print(j + " ");
+    public T[] sort(T[] obj) { return mergeSort(obj); }
+
+    private T[] mergeSort(T[] objArray) {
+        int len = objArray.length;
+        if (len < 2) return objArray;
+
+        int midIndex = len / 2;
+        T[] leftArray = (T[]) new Comparable[midIndex];
+        T[] rightArray = (T[]) new Comparable[len - midIndex];
+
+        for (int i = 0; i < midIndex; i++) leftArray[i] = objArray[i];
+        for (int j = midIndex; j < len; j++) rightArray[j - midIndex] = objArray[j];
+
+        mergeSort(leftArray); mergeSort(rightArray);
+
+        return merge(leftArray, rightArray, objArray);
     }
 
-    public static void main(String[] args) {
-        MergeSort main = new MergeSort();
-        int[] arr = main.randomNums(15);
-        System.out.println("Before Sort\n");
-        main.printArr(arr);
-        System.out.println("\nAfter Sort\n");
-        main.mergeSort(arr);
-        main.printArr(arr);
-    }
-
-    private int[] mergeSort(int[] numArr) {
-        int len = numArr.length;
-        if (len < 2) return numArr;
-
-        int midPoint = len / 2;
-        int[] leftArray = new int[midPoint];
-        int[] rightArray = new int[len - midPoint];
-
-        for (int i = 0; i < midPoint; i++) leftArray[i] = numArr[i];
-        for (int i = midPoint; i < len; i++) rightArray[i - midPoint] = numArr[i];
-
-        mergeSort(leftArray);
-        mergeSort(rightArray);
-
-        return merge(leftArray, rightArray, numArr);
-    }
-
-    private int[] merge(int[] leftArray, int[] rightArray, int[] numArr){
+    private T[] merge(T[] leftArr, T[] rightArr, T[] objArr) {
         int leftCounter = 0, rightCounter = 0, ogCounter = 0;
-        int leftSize = leftArray.length, rightSize = rightArray.length;
-        int len = numArr.length;
+        int leftSize = leftArr.length, rightSize = rightArr.length;
 
         while (leftCounter < leftSize && rightCounter < rightSize) {
-            if (leftArray[leftCounter] <= rightArray[rightCounter]){
-                numArr[ogCounter] = leftArray[leftCounter++];
-            } else numArr[ogCounter] = rightArray[rightCounter++];
+            if (leftArr[leftCounter].compareTo(rightArr[rightCounter]) <= 0)
+                objArr[ogCounter] = leftArr[leftCounter++];
+            else objArr[ogCounter] = rightArr[rightCounter++];
             ogCounter++;
         }
 
-        while (leftCounter < leftSize) {
-            numArr[ogCounter++] = leftArray[leftCounter++];
-        }
+        while (leftCounter < leftSize) objArr[ogCounter++] = leftArr[leftCounter++];
+        while (rightCounter < rightSize) objArr[ogCounter++] = rightArr[rightCounter++];
 
-        while (rightCounter < rightSize) {
-            numArr[ogCounter++] = rightArray[rightCounter++];
-        }
-
-        return numArr;
+        return objArr;
     }
 }
---
+++ src/main/java/org/sorts/MergeSortImpl.java
@@ -0,0 +1,33 @@
+package org.sorts;
+
+import java.util.Random;
+
+public class MergeSortImpl {
+
+    private final Random rand;
+
+    public MergeSortImpl() {
+        this.rand = new Random();
+    }
+
+    public Integer[] initInitArray(int cap) {
+        Integer[] arr = new Integer[cap];
+        for (int i = 0; i < cap; i++) arr[i] = rand.nextInt(cap);
+        return arr;
+    }
+
+    private <T> void printArray(T[] arr) {
+        for (T e : arr) System.out.print(e.toString() + " ");
+    }
+
+    public static void main(String[] args) {
+        MergeSortImpl mergeSortImpl = new MergeSortImpl();
+        MergeSort<Integer> integerMergeSort = new MergeSort<>();
+        Integer[] arr = mergeSortImpl.initInitArray(30);
+        System.out.println("*** Before Sort ***");
+        mergeSortImpl.printArray(arr);
+        integerMergeSort.sort(arr);
+        System.out.println("\n\n*** After Sort ***");
+        mergeSortImpl.printArray(arr);
+    }
+}
